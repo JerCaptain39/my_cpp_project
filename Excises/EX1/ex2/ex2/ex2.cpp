b6245a1915b00e8e0b60e67583e60ffb51336907
JeremyX <JerCaptain39@outlook.com>
2018-01-19 17:28:59 +0800

    练习

---
+++ Excises/EX1/ex2/ex2/ex2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+//int main()
+//{
+//	cout << "请输出名字：";
+//	cout << endl;
+//	cout << "What? I don't know what C++ is!";
+//	cout << "It is so diffcult! I'll never get it!";
+//	cin.get();
+//}
+
+int main()
+{
+	cout << "请输出名字：";
+	cout << endl;
+	cin.get();
+	return 0;//函数执行到return 后会退出函数返回调用位置。
+	cout << "What? I don't know what C++ is!";
+	cout << "It is so diffcult! I'll never get it!";
+	cin.get();
+}
---
+++ Excises/EX1/ex2/ex2/ex3.cpp
@@ -0,0 +1,12 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	cout << "ÇëÊä³öÃû×Ö£º";
+//	cout << endl;
+//	//return 0;
+//	/*cout << "What? I don't know what C++ is!";
+//	cout << "It is so diffcult! I'll never get it!";
+//	cin.get();*/
+//}
